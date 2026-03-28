5d8bb6465b2966e7e166c0493c34415c9ac7d9ec
Bilal2453 <belal2453@gmail.com>
2026-03-28 20:49:40 +0300

    ci: add new Dockerfile deployments

--- containers/build.sh
+++ containers/build.sh
@@ -1,12 +1,29 @@
 #!/bin/sh
 set -e
 
-echo "Making customized alpine image..."
-docker build -t creationix/lit lit-runtime
+BUILDER=${BUILDER:-"buildah"}
 
-echo "Run a test app..."
-docker build test-app -t test-app
-docker run --rm -it -p 8080:8080 test-app
+LIT_VERSION=${LIT_VERSION:-"3.9.0"}
+LUVI_VERSION=${LUVI_VERSION:-"2.15.0"}
+LUVIT_VERSION=${LUVIT_VERSION:-"2.18.1"}
 
-echo "Done. Uploading to docker hub..."
-docker push creationix/lit
+echo "Building an OCI Alpine image of my-luvit/lit"
+$BUILDER build \
+  --build-arg LIT_VERSION="${LIT_VERSION}" \
+  --build-arg LUVI_VERSION="${LUVI_VERSION}" \
+  --build-arg LUVIT_VERSION="${LUVIT_VERSION}" \
+  --target lit \
+  -t my-luvit/lit:"${LIT_VERSION}" \
+  -t my-luvit/lit:latest \
+  ./lit-runtime
+
+echo "Building an OCI Alpine image of my-luvit/lit-server"
+$BUILDER build \
+  --build-arg LIT_VERSION="${LIT_VERSION}" \
+  --build-arg LUVI_VERSION="${LUVI_VERSION}" \
+  --build-arg LUVIT_VERSION="${LUVIT_VERSION}" \
+  -t my-luvit/lit-server:"${LIT_VERSION}" \
+  -t my-luvit/lit-server:latest \
+  ./lit-server
+
+echo "Done."
