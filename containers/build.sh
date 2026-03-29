6578997bc8e806b1e89df558e2086643b387c3c3
Bilal2453 <belal2453@gmail.com>
2026-03-29 19:02:54 +0300

    cI: add github action to push to ghcr

--- containers/build.sh
+++ containers/build.sh
@@ -2,6 +2,7 @@
 set -e
 
 BUILDER=${BUILDER:-"buildah"}
+REGISTRY=${BUILDER:-"ghcr.io/"}
 
 LIT_VERSION=${LIT_VERSION:-"3.9.0"}
 LUVI_VERSION=${LUVI_VERSION:-"2.15.0"}
@@ -17,6 +18,36 @@ $BUILDER build \
   -t my-luvit/lit:latest \
   ./lit-runtime
 
+echo "Building an OCI Alpine image of my-luvit/luvi"
+$BUILDER build \
+  --build-arg LIT_VERSION="${LIT_VERSION}" \
+  --build-arg LUVI_VERSION="${LUVI_VERSION}" \
+  --build-arg LUVIT_VERSION="${LUVIT_VERSION}" \
+  --target luvi \
+  -t my-luvit/luvi:"${LUVI_VERSION}" \
+  -t my-luvit/luvi:latest \
+  ./lit-runtime
+
+echo "Building an OCI Alpine image of my-luvit/luvit"
+$BUILDER build \
+  --build-arg LIT_VERSION="${LIT_VERSION}" \
+  --build-arg LUVI_VERSION="${LUVI_VERSION}" \
+  --build-arg LUVIT_VERSION="${LUVIT_VERSION}" \
+  --target luvit \
+  -t my-luvit/luvit:"${LUVIT_VERSION}" \
+  -t my-luvit/luvit:latest \
+  ./lit-runtime
+
+echo "Building an OCI Alpine image of my-luvit/all"
+$BUILDER build \
+  --build-arg LIT_VERSION="${LIT_VERSION}" \
+  --build-arg LUVI_VERSION="${LUVI_VERSION}" \
+  --build-arg LUVIT_VERSION="${LUVIT_VERSION}" \
+  --target all \
+  -t my-luvit/all:"${LUVIT_VERSION}" \
+  -t my-luvit/all:latest \
+  ./lit-runtime
+
 echo "Building an OCI Alpine image of my-luvit/lit-server"
 $BUILDER build \
   --build-arg LIT_VERSION="${LIT_VERSION}" \
@@ -25,5 +56,13 @@ $BUILDER build \
   -t my-luvit/lit-server:"${LIT_VERSION}" \
   -t my-luvit/lit-server:latest \
   ./lit-server
+
+if [ "$1" = "--push" ]; then
+  echo "Pushing built images to registry ${REGISTRY}"
+  $BUILDER push "${REGISTRY}/my-luvit/lit:latest" "${REGISTRY}/my-luvit/lit:${LIT_VERSION}"
+  $BUILDER push "${REGISTRY}/my-luvit/luvi:latest" "${REGISTRY}/my-luvit/luvi:${LUVI_VERSION}"
+  $BUILDER push "${REGISTRY}/my-luvit/luvit:latest" "${REGISTRY}/my-luvit/luvit:${LUVIT_VERSION}"
+  $BUILDER push "${REGISTRY}/my-luvit/all:latest" "${REGISTRY}/my-luvit/all:${LUVIT_VERSION}"
+fi
 
 echo "Done."
